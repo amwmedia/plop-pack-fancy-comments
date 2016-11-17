f09e7fc352ba953c4fc34271600881732a4bac65
Andrew Worcester <andrew@amwmedia.com>
2016-11-17 14:21:21 -0500

    minor update to defaults and comment start/end handling

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ module.exports = function (plop, config) {
 
 	// setup config defaults
 	const cfg = Object.assign({
-		prefix: '',
+		prefix: 'js-',
 		upperCaseHeaders: false,
-		blockStart: '/*',
-		blockEnd: '*/',
-		inlineStart: '//'
+		commentStart: '/*',
+		commentEnd: '*/',
 	}, config || {});
 
 	plop.setDefaultInclude({ helpers: true });
@@ -25,7 +24,7 @@ module.exports = function (plop, config) {
 
 		const border = '==';
 		const padding = '  ';
-		return `${cfg.inlineStart} ${border}${padding}END ${text}${padding}${border}`;
+		return `${cfg.commentStart}${border}${padding}END ${text}${padding}${border}${cfg.commentEnd}`;
 	});
 
 	function multiLineHeader(text) {
@@ -34,12 +33,12 @@ module.exports = function (plop, config) {
 		const padding = '  ';
 		const lines = text.split('\n');
 		const longestLine = Math.max.apply(null, lines.map(line => line.length));
-		var out = `${cfg.blockStart} ${repeat('=', longestLine + (border.length * 2) + (padding.length * 2))}`;
-		out += `\n${repeat(' ', cfg.blockStart.length)} ${border}${padding}`;
+		var out = `${cfg.commentStart}${repeat('=', longestLine + (border.length * 2) + (padding.length * 2) - cfg.commentStart.length)}`;
+		out += `\n${border}${padding}`;
 		out += lines.map(line => (line.length < longestLine ? line + repeat(' ', longestLine - line.length) : line))
-			.join(`${padding}${border}\n${repeat(' ', cfg.blockStart.length)} ${border}${padding}`);
+			.join(`${padding}${border}\n${border}${padding}`);
 		out += `${padding}${border}\n`;
-		out += `${repeat(' ', cfg.blockStart.length)} ${repeat('=', longestLine + (border.length * 2) + (padding.length * 2))} ${cfg.blockEnd}`;
+		out += `${repeat('=', longestLine + (border.length * 2) + (padding.length * 2) - cfg.commentEnd.length)}${cfg.commentEnd}`;
 
 		return out;
 	}
